fedf7fec0cc2b155f8e4422e11f35898f5d10313
journnie <jhyun095@gmail.com>
2024-03-19 12:24:08 +0900

    conflict solved

--- test.py
+++ test.py
@@ -3,11 +3,8 @@
 for i in range(100):
     print(i)
 
-<<<<<<< HEAD
 # 지현
 for j in range(11, 21):
     print(j)
-=======
 for x in range(31, 41):
     print(x, end = ' ')
->>>>>>> origin/f/print_number
